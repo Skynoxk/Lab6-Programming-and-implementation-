ac83f9226356a5287e16c7673ed7e0b11ff6e735
Meas Ratanakviphou <83325447+Skynoxk@users.noreply.github.com>
2025-01-16 07:52:59 +0700

    Delete src directory

--- src/tempCodeRunnerFile.java
+++
@@ -1,3 +0,0 @@
-double price[] = { 100.0, 50.0, 150.0, 200.0, 250.0 };
-        String name[] = { "Product 1", "Product 2", "Product 3", "Product 4", "Product 5" };
-        String ID[] = { "ID1", "ID2", "ID3", "ID4", "ID5" };
